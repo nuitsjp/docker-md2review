f65f3871dd3c0bbd1b2a079388d20932fa195684
Atsushi Nakamura <nuits.jp@live.jp>
2018-06-27 23:47:56 +0900

    makereview.shでコンテナのタグを明示的に指定するよう修正

--- makereview.sh
+++ makereview.sh
@@ -1,1 +1,1 @@
-docker run --rm -v `pwd`/.:/work nuitsjp/md2review /bin/sh -c "cd /work && md2review README.md > README.re"
+docker run --rm -v `pwd`/.:/work nuitsjp/md2review:1.12.0 /bin/sh -c "cd /work && md2review README.md > README.re"
